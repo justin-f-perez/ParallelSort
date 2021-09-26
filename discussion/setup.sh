d1a170e6e6a6e205e8b38f62cc7326d71c194b94
Justin Perez <justin-perez@uiowa.edu>
2021-09-25 20:09:41 -0500

    fix setup/start scripts and make them more portable

--- discussion/setup.sh
+++ discussion/setup.sh
@@ -1,11 +1,17 @@
+#!/usr/bin/env bash
 set -e           # fail/exit if any command in the script yields an exit error code
 set -u           # fail/exit if attempting to expand an env var that isn't set
 set -o pipefail  # fail/exit if a pipeline fails
-set -x           # print commands to stdout as they run
 
-# clears output of python notebooks on commit to prevent noisey diffs
-cp pre-commit ../.git/hooks/
+#DISABLED: too noisy, outputs all the stuff inside of the virtual environment activate script too
+#set -x           # print commands to stdout as they run  
 
+
+cd `dirname $0`  # change into the directory of this script just incase it was invoked from outside of it
+echo "working directory: `pwd`"
+echo "Creating a python virtual environment to install dependencies into..."
 python3 -m venv .venv
+echo "Activating virtual environment so 'pip install' commands will run inside the virtual environment..."
 source .venv/bin/activate
-pip install -r requirements.txt -qq
+echo "Installing dependencies (this may take a few minutes)"
+pip install -r requirements.txt
--- discussion/start.sh
+++ discussion/start.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`  # change into the directory of this script just incase it was invoked from outside of it
+echo "working directory: `pwd`"
+echo "Activating virtual environment where python dependencies are installed..."
 source .venv/bin/activate
 jupyter lab
